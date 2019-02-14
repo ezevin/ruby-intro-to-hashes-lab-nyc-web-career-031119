4f9b40737cda055c6e70c905259ab0df393f0008
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-14 20:13:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,6 +39,6 @@ def monopoly_with_fourth_tier
   	                                :b_and_o_railroad => {}, 
   	                                :shortline => {}}
   	          
-  	  
+  }
   }
 end
